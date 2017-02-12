6d5af37f921a0b624c3f84d698a7cbcaac29cf31
Tom Hsieh <thsie004@hammer.cs.ucr.edu>
2017-02-12 05:35:27 -0800

    fixed sigtstp_handler's kill call

--- tsh.c
+++ tsh.c
@@ -399,7 +399,7 @@ void sigtstp_handler(int sig)
 {
     pid_t pid;
     if((pid = fgpid(jobs)) != 0){
-        kill(pid, SIGTSTP);
+        kill(-pid, SIGTSTP);
         getjobpid(jobs, pid)->state = ST;
         printf("Job [%d] (%d) stopped by signal %d\n", pid2jid(pid), pid, sig);
     }
